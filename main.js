d9785239cc4a815f86c1b27898f7b36d00856235
unknown <mrbc1190@gmail.com>
2018-04-25 16:05:40 -0600

    Proyecto

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	var altura = $('.menu1').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu1').addClass('menu-fixed');
+		} else {
+			$('.menu1').removeClass('menu-fixed');
+		}
+	});
+
+});
