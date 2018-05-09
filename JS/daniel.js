4e0f33fefb3c8adb13f4c43a1cea49f8c7e04c69
cequeda <quesada04@gmail.com>
2018-05-09 09:47:03 -0600

    fd

--- JS/daniel.js
+++ JS/daniel.js
@@ -1,3 +1,5 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage();
+$(document).ready(function(){
+	$("#demosMenu").change(function(){
+	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
+	});
 });
